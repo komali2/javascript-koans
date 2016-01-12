7e6e7032e82ac70880106767dc607716696470e0
Caleb Rogers <rogersjcaleb@gmail.com>
2016-01-12 11:13:57 -0600

    corrected aboutarrays

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -3,12 +3,12 @@ describe("About Arrays", function() {
   //We shall contemplate truth by testing reality, via spec expectations.
   it("should create arrays", function() {
     var emptyArray = [];
-    expect(typeof(emptyArray)).toBe(object); //A mistake? - http://javascript.crockford.com/remedial.html
+    expect(typeof(emptyArray)).toBe("object"); //A mistake? - http://javascript.crockford.com/remedial.html
     expect(emptyArray.length).toBe(0);
 
     var multiTypeArray = [0, 1, "two", function () { return 3; }, {value1: 4, value2: 5}, [6, 7]];
     expect(multiTypeArray[0]).toBe(0);
-    expect(multiTypeArray[2]).toBe(2);
+    expect(multiTypeArray[2]).toBe("two");
     expect(multiTypeArray[3]()).toBe(3);
     expect(multiTypeArray[4].value1).toBe(4);
     expect(multiTypeArray[4]["value2"]).toBe(5);
@@ -47,12 +47,12 @@ describe("About Arrays", function() {
     var array = ["peanut", "butter", "and", "jelly"];
 
     expect(array.slice(0, 1)).toEqual(["peanut"]);
-    expect(array.slice(0, 2)).toEqual("peanut","butter");
-    expect(array.slice(2, 2)).toEqual();
-    expect(array.slice(2, 20)).toEqual("and","jelly");
-    expect(array.slice(3, 0)).toEqual();
-    expect(array.slice(3, 100)).toEqual("jelly");
-    expect(array.slice(5, 1)).toEqual();
+    expect(array.slice(0, 2)).toEqual(["peanut","butter"]);
+    expect(array.slice(2, 2)).toEqual([]);
+    expect(array.slice(2, 20)).toEqual(["and","jelly"]);
+    expect(array.slice(3, 0)).toEqual([]);
+    expect(array.slice(3, 100)).toEqual(["jelly"]);
+    expect(array.slice(5, 1)).toEqual([]);
   });
 
   it("should know array references", function () {
@@ -66,7 +66,7 @@ describe("About Arrays", function() {
 
     var assignedArray = array;
     assignedArray[5] = "changed in assignedArray";
-    expect(array[5]).toBe("five");
+    expect(array[5]).toBe("changed in assignedArray");
 
     var copyOfArray = array.slice();
     copyOfArray[3] = "changed in copyOfArray";
